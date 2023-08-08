420fbbc164507b4395900a7a821c0685d2dfaf8c
Daniel Yan <doraniel2000@gmail.com>
2023-08-08 15:45:24 -0300

    Fix Intersection Error

--- src/hooks/useOnScreen.tsx
+++ src/hooks/useOnScreen.tsx
@@ -4,18 +4,21 @@ import { RefObject, useState, useMemo, useEffect } from 'react'
 export default function useOnScreen(ref: RefObject<HTMLElement>) {
   const [isIntersecting, setIntersecting] = useState(false)
 
-  const observer = useMemo(
-    () =>
-      new IntersectionObserver(([entry]) =>
-        setIntersecting(entry.isIntersecting),
-      ),
-    [ref],
-  )
-
   useEffect(() => {
-    observer.observe(ref.current!)
-    return () => observer.disconnect()
-  }, [])
+    const observer = new IntersectionObserver(([entry]) => {
+      setIntersecting(entry.isIntersecting)
+    })
+
+    if (ref.current) {
+      observer.observe(ref.current)
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current)
+      }
+    }
+  }, [ref])
 
   return isIntersecting
 }
